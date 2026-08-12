b963cdda419451e2a7d6b5109ed681ed62e1deb1
Shravankotagi <shravankotagi314@gmail.com>
2026-08-12 15:34:23 +0530

    chore: untrack test scripts

--- test-gemini.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: '../.env' });
-const { extractFromText } = require('./src/gemini');
-
-async function test() {
-  const result = await extractFromText('need 10 MT HR coil 2mm by Friday, customer ABC Fabricators');
-  console.log('Result:', JSON.stringify(result, null, 2));
-}
-test();
